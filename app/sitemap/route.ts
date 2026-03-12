0886c8903d7a47d3acc9fbc2d549d7672e6741c4
Roberth González <roberth.gonzalez@manifiesto.biz>
2026-03-12 16:06:10 +0100

    Fix sitemap route conflict

--- app/sitemap/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET(request: Request) {
-  const url = new URL(request.url);
-  url.pathname = "/sitemap.xml";
-  return NextResponse.redirect(url, 308);
-}
